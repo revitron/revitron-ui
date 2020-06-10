09538875d3cde7aa68526b575807bc7ac9046d58
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-06-10 18:25:22 +0200

    Create Move Links_script.py

---
+++ Revitron.tab/Revitron.panel/Utils.pulldown/Move Links.pushbutton/Move Links_script.py
@@ -0,0 +1,19 @@
+import revitron
+from rpw.ui.forms import select_file
+from rpw.ui.forms.resources import *
+
+__context__ = 'zerodoc'
+
+def select_folder(title):
+    form = Forms.FolderBrowserDialog()
+    form.Description = title
+    if form.ShowDialog() == Forms.DialogResult.OK:
+        return form.SelectedPath
+
+host = select_file('Revit Model (*.rvt)|*.rvt')
+source = select_folder('Select source directory for link structure')
+target = select_folder('Select target directory for link structure')
+
+print('Moving Links ...')
+revitron.TransmissionData(host).moveLinksOnDisk(source, target)
+print('Done')
