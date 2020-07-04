04f4dc69cecb0026dcec3b360daf6a4fa8ab69c2
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-07-04 20:07:23 +0200

    Create Search and Replace in Link Paths_script.py

---
+++ Revitron.tab/Revitron.panel/Utils.pulldown/Search and Replace in Link Paths.pushbutton/Search and Replace in Link Paths_script.py
@@ -0,0 +1,12 @@
+import revitron
+from rpw.ui.forms import TextInput
+from rpw.ui.forms import select_file
+
+__context__ = 'zerodoc'
+
+host = select_file('Revit Model (*.rvt)|*.rvt')
+search = TextInput('Search')
+replace = TextInput('Replace')
+revitron.TransmissionData(host).replaceInPath(search, replace)
+
+print('Done')
