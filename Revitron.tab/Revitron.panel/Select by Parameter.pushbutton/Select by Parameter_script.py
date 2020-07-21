d1bf01b4d46b015059c9aafaee4be4e6d0d21966
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-07-21 17:02:48 +0200

    Update Select by Parameter_script.py

--- Revitron.tab/Revitron.panel/Select by Parameter.pushbutton/Select by Parameter_script.py
+++ Revitron.tab/Revitron.panel/Select by Parameter.pushbutton/Select by Parameter_script.py
@@ -5,6 +5,7 @@ import System.Windows
 
 defaultParameter = False
 parameters = revitron.ParameterNameList().get()
+selection = revitron.Selection.get()
 
 if 'Family and Type' in parameters:
     defaultParameter = 'Family and Type'
@@ -16,20 +17,27 @@ components = [
     TextBox('search'),
     Separator(),
     CheckBox('invert', 'Invert Selection', default=False),
-    CheckBox('viewOnly', 'This view only', default=False),
-    Button('Select', Width=100, HorizontalAlignment=System.Windows.HorizontalAlignment.Right)
+    CheckBox('viewOnly', 'This view only', default=False)
 ]
+
+if selection:
+    components.append(CheckBox('selection', 'Search in selection only', default=True))
+    
+components.append(Button('Select', Width=100, HorizontalAlignment=System.Windows.HorizontalAlignment.Right))
 
 form = FlexForm('Select by Parameter', components)
 form.show()
 
 if 'search' in form.values:
 
-    viewId = False
+    scope = False
 
-    if form.values['viewOnly']:
-        viewId = revitron.ACTIVEVIEW.Id
+    if 'selection' in form.values and form.values['selection']:
+        scope = selection
+    else:
+        if form.values['viewOnly']:
+            scope = revitron.ACTIVEVIEW.Id
 
-    ids = revitron.Filter(viewId).noTypes().byStringContains(form.values['parameter'], form.values['search'], form.values['invert']).getElementIds()
+    ids = revitron.Filter(scope).noTypes().byStringContains(form.values['parameter'], form.values['search'], form.values['invert']).getElementIds()
 
     revitron.Selection.set(ids)
