b91028452f116e01743089bbf6adc5390bbe77fe
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-09-23 12:04:48 +0200

    Update Export Sheets as DWG_script.py

--- Revitron.tab/Revitron.panel/Export.pulldown/Export Sheets as DWG.pushbutton/Export Sheets as DWG_script.py
+++ Revitron.tab/Revitron.panel/Export.pulldown/Export Sheets as DWG.pushbutton/Export Sheets as DWG_script.py
@@ -1,5 +1,7 @@
 import revitron
+import os
 from revitron import _
+from pyrevit import script
 from collections import defaultdict
 
 config = revitron.DocumentConfigStorage().get('revitron.export', defaultdict())
@@ -11,7 +13,19 @@ sheets = revitron.Selection().get()
 if not sheets:
     sheets = [revitron.ACTIVEVIEW]
 
+dirs = []
+
 for sheet in sheets:
-	exporter.exportSheet(sheet, 
-                      	 config.get('Sheet_Export_Directory'), 
-						 config.get('Sheet_Naming_Template'))
+	path = exporter.exportSheet(sheet, 
+                      		    config.get('Sheet_Export_Directory'), 
+						 	    config.get('Sheet_Naming_Template'))
+	if path:
+		dirs.append(os.path.dirname(path))
+		script.get_output().print_html(':smiling_face: Exported <em>{}</em>'.format(os.path.basename(path)))
+	else:
+		script.get_output().print_html(':pouting_face: Error exporting <em>{}</em>'.format(os.path.basename(path)))
+
+dirs = list(set(dirs))
+
+for d in dirs:
+    script.show_folder_in_explorer(d)
