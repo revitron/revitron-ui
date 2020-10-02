dfa6a3b75dec50f85dae62fd5f5b33a9426a2ab0
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-10-03 00:05:24 +0200

    Update About_script.py

--- Revitron.tab/Revitron.panel/About.splitbutton/About.pushbutton/About_script.py
+++ Revitron.tab/Revitron.panel/About.splitbutton/About.pushbutton/About_script.py
@@ -4,10 +4,14 @@ from pyrevit import output
 
 __context__ = 'zero-doc'
 
-output.set_stylesheet(revitron.LIB_DIR + '\\css\\output.css')
+style = 'body {padding: 20px 40px; color: #121212;} img {max-width: 500px; padding-bottom: 20px} span {display: block; text-align: center;}'
+output.get_output().add_style(style)
+output.get_output().set_width(500)
+output.get_output().set_height(500)
+output.get_output().center()
 out = script.get_output()
-out.print_image(revitron.LIB_DIR + '/svg/revitron-white.svg')
-out.print_html('<strong>Revitron</strong><br><br>A Revit API wrapper written in Python.<br>' +\
+out.print_image(revitron.LIB_DIR + '/svg/revitron-readme.svg')
+out.print_html('<h3>Revitron</h3>A Revit API wrapper written in Python.<br>' +\
 	'Check out the <a href="https://revitron-ui.readthedocs.io/">documentation</a>, ' +\
     'the <a href="https://revitron.readthedocs.io/">API reference</a><br>' +\
     'and the repositories on <a href="https://github.com/revitron">GitHub</a> for more infos.<br><br>' +\
