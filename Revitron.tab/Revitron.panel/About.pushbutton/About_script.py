e9c58b7ed4f8a63a25ba48c5e64e064746984e3f
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-10-21 20:56:21 +0200

    Update About_script.py

--- Revitron.tab/Revitron.panel/About.pushbutton/About_script.py
+++ Revitron.tab/Revitron.panel/About.pushbutton/About_script.py
@@ -1,14 +1,19 @@
 import revitron
 from pyrevit import script
 from pyrevit import output
+import os
 
-style = 'body {padding: 20px 40px; color: #121212;} img {max-width: 500px; padding-bottom: 20px} span {display: block; text-align: center;}'
+parent = os.path.dirname 
+
+svg = parent(parent(parent(parent(__file__)))) + '/svg/revitron-about.svg'
+
+style = 'img {max-width: 500px; padding: 25px 0} span {display: block; text-align: center;}'
 output.get_output().add_style(style)
-output.get_output().set_width(510)
-output.get_output().set_height(510)
+output.get_output().set_width(500)
+output.get_output().set_height(500)
 output.get_output().center()
 out = script.get_output()
-out.print_image(revitron.LIB_DIR + '/svg/revitron-readme.svg')
+out.print_image(svg)
 out.print_html('<h2>Revitron</h2>A Revit API wrapper written in Python.<br>' +\
 	'Check out the <a href="https://revitron-ui.readthedocs.io/">documentation</a>, ' +\
     'the <a href="https://revitron.readthedocs.io/">API reference</a><br>' +\
