8680d5ae0e4cb369f6479a2253c380aada440cb7
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-10-22 20:24:08 +0200

    Improved icons and meta data

--- Revitron.tab/Revitron.panel/Export.pulldown/ExportSettings.pushbutton/ExportSettings_script.py
+++ Revitron.tab/Revitron.panel/Export.pulldown/ExportSettings.pushbutton/ExportSettings_script.py
@@ -2,57 +2,59 @@ import revitron
 from revitron import _
 from rpw.ui.forms import FlexForm, TextBox, Button, Label, Separator, ComboBox
 from collections import defaultdict
+from pyrevit import script
 import System.Windows
 
 def addFields(components, fields):
-    for field in fields:
-        if field == '---':
-            components.append(Separator())
-        else:
-        	key = revitron.String.sanitize(field)
-        	components.append(Label(field))
-        	components.append(TextBox(key, Text=config.get(key)))
-    return components
+	for field in fields:
+		if field == '---':
+			components.append(Separator())
+		else:
+			key = revitron.String.sanitize(field)
+			components.append(Label(field))
+			components.append(TextBox(key, Text=config.get(key)))
+	return components
+
+def openHelp(sender, e):
+	script.open_url('https://revitron-ui.readthedocs.io/en/latest/tools/export.html')
 
 if not revitron.Document().isFamily():
-    
-    config = revitron.DocumentConfigStorage().get('revitron.export', defaultdict())
-    
-    components = addFields([], 
-    [
-        'Sheet Export Directory',
-        'Sheet Naming Template',
-        'Sheet Size Parameter Name',
-        'Default Sheet Size',
-        'Sheet Orientation Parameter Name'
-    ])
+	
+	config = revitron.DocumentConfigStorage().get('revitron.export', defaultdict())
+	
+	components = addFields([], 
+	[
+		'Sheet Export Directory',
+		'Sheet Naming Template',
+		'Sheet Size Parameter Name',
+		'Default Sheet Size',
+		'Sheet Orientation Parameter Name'
+	])
 
-    orientationField = 'Default Sheet Orientation'
-    orientationKey = revitron.String.sanitize(orientationField)
-    orientations = ['Landscape', 'Portrait']
-    default = orientations[0]
-    if config.get(orientationKey) in orientations:
-        default = config.get(orientationKey)
-    components.append(Label(orientationField))
-    components.append(ComboBox(orientationKey, orientations, default=default))
-    
-    components = addFields(components, 
-    [
-        '---',
-        'PDF Printer Address',
-        'PDF Temporary Output Path',
-        '---',
-        'DWG Export Setup'
-    ])
-    
-    components.append(Label(''))
-    components.append(Button('Save', Width=100, HorizontalAlignment=System.Windows.HorizontalAlignment.Right))
-    
-    form = FlexForm('Revitron PDF and DWG Export Settings', components)
-    form.show()
-          
-    if form.values: 
-        revitron.DocumentConfigStorage().set('revitron.export', form.values)
-       
-       
-        
+	orientationField = 'Default Sheet Orientation'
+	orientationKey = revitron.String.sanitize(orientationField)
+	orientations = ['Landscape', 'Portrait']
+	default = orientations[0]
+	if config.get(orientationKey) in orientations:
+		default = config.get(orientationKey)
+	components.append(Label(orientationField))
+	components.append(ComboBox(orientationKey, orientations, default=default))
+	
+	components = addFields(components, 
+	[
+		'---',
+		'PDF Printer Address',
+		'PDF Temporary Output Path',
+		'---',
+		'DWG Export Setup'
+	])
+	
+	components.append(Label(''))
+	components.append(Button('Open Documentation', on_click=openHelp))
+	components.append(Button('Save'))
+	
+	form = FlexForm('PDF and DWG Export Settings', components)
+	form.show()
+		  
+	if form.values: 
+		revitron.DocumentConfigStorage().set('revitron.export', form.values)
