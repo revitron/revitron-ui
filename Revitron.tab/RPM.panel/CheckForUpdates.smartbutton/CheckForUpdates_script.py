2ba4f2b03ddb689aaefb2a55df337275a1051895
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-10-22 12:39:06 +0200

    Update CheckForUpdates_script.py

--- Revitron.tab/RPM.panel/CheckForUpdates.smartbutton/CheckForUpdates_script.py
+++ Revitron.tab/RPM.panel/CheckForUpdates.smartbutton/CheckForUpdates_script.py
@@ -1,12 +1,15 @@
 """
 Check for pyRevit core and extension updates and optionally install them. 
 """
+import os
 from rpm.system.ui import UI
 from pyrevit.coreutils.ribbon import ICON_LARGE
 
 __context__ = 'zero-doc'
 
 def __selfinit__(script_cmp, ui_button_cmp, __rvt__):
+	if os.system('git --version') != 0:
+		return False
 	if UI.checkUpdates():
 		ui_button_cmp.set_title('Install\nUpdates')
 		update_icon = script_cmp.get_bundle_file('icon-has-updates.png')
