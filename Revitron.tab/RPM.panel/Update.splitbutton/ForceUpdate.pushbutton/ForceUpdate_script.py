9393f70793051e43c2ad8ff1dc8b534ccbe21136
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-10-27 12:15:10 +0100

    Update ForceUpdate_script.py

--- Revitron.tab/RPM.panel/Update.splitbutton/ForceUpdate.pushbutton/ForceUpdate_script.py
+++ Revitron.tab/RPM.panel/Update.splitbutton/ForceUpdate.pushbutton/ForceUpdate_script.py
@@ -2,10 +2,19 @@ from rpm.system.ui import UI
 from rpm.system.update import Update 
 from rpm.system.session import Session
 from pyrevit import script
+from pyrevit import forms
 
-out = script.get_output()
-UI.printLogo()
-UI.printTitle()
-Update.extensions(True)
-out.print_html('<br><br>Update has finished. Reloading ...<br><br>')
-Session.reload() 
+
+update = 'Discard all changes and update now'
+cancel = 'Cancel'
+res = forms.alert('Updating will discard all local changes in your extension repositories!',
+				  title = 'Force Extension Update',
+				  options = [update, cancel])
+
+if res == update:
+	out = script.get_output()
+	UI.printLogo()
+	UI.printTitle()
+	#Update.extensions(True)
+	out.print_html('<br><br>Update has finished. Reloading ...<br><br>')
+	Session.reload() 
