7add6c595857c8dcbd108aba0dcf71d7519e6177
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-10-27 14:36:40 +0100

    Update ForceUpdate_script.py

--- Revitron.tab/RPM.panel/Update.splitbutton/ForceUpdate.pushbutton/ForceUpdate_script.py
+++ Revitron.tab/RPM.panel/Update.splitbutton/ForceUpdate.pushbutton/ForceUpdate_script.py
@@ -15,6 +15,6 @@ if res == update:
 	out = script.get_output()
 	UI.printLogo()
 	UI.printTitle()
-	#Update.extensions(True)
+	Update.extensions(True)
 	out.print_html('<br><br>Update has finished. Reloading ...<br><br>')
 	Session.reload() 
