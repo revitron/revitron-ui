ee0fc14e31d57f7c4e34eea2ce70133b10a32b91
Marc Anton Dahmen <revitron@marcdahmen.de>
2021-01-11 16:47:20 +0100

    Update About_script.py

--- Revitron.tab/Revitron.panel/About.pushbutton/About_script.py
+++ Revitron.tab/Revitron.panel/About.pushbutton/About_script.py
@@ -7,17 +7,16 @@ parent = os.path.dirname
 
 svg = parent(parent(parent(parent(__file__)))) + '/svg/revitron-about.svg'
 
-style = 'img {max-width: 500px; padding: 25px 0} span {display: block; text-align: center;}'
+style = 'img {max-width: 30rem} span {display: block; text-align: center;}'
 output.get_output().add_style(style)
 output.get_output().set_width(500)
-output.get_output().set_height(500)
-output.get_output().center()
+output.get_output().set_height(440)
 out = script.get_output()
 out.print_image(svg)
-out.print_html('<h2>Revitron</h2>A Revit API wrapper written in Python.<br>' +\
+out.print_html('<div style="padding: 0 1rem"><h2>Revitron</h2>A Revit API wrapper written in Python.<br>' +\
 	'Check out the <a href="https://revitron-ui.readthedocs.io/">documentation</a>, ' +\
     'the <a href="https://revitron.readthedocs.io/">API reference</a><br>' +\
     'and the repositories on <a href="https://github.com/revitron">GitHub</a> for more infos.<br><br>' +\
     '&copy; Copyright 2020, Marc Anton Dahmen &mdash; MIT license<br>' +\
-    'All icons except for the Revitron logo by <a href="https://icons8.com">Icons8</a>')
+    'All icons except for the Revitron logo by <a href="https://icons8.com">Icons8</a></div>')
 
