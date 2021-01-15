adf0a452927776eef655ec23c6cd8b173971430e
Marc Anton Dahmen <revitron@marcdahmen.de>
2021-01-15 13:31:12 +0100

    Added JoinWithIntersecting button

---
+++ Revitron.tab/Revitron.panel/Model.pulldown/JoinWithIntersecting.pushbutton/JoinWithIntersecting_script.py
@@ -0,0 +1,22 @@
+import revitron 
+from revitron import _
+from pyrevit import script
+
+output = script.get_output()
+
+transaction = revitron.Transaction()
+
+for element in revitron.Selection.get():
+	_element = _(element)
+	if _element.isNotOwned():
+		for intersected in revitron.Filter().byIntersection(element).getElements():
+			if not revitron.DB.JoinGeometryUtils.AreElementsJoined(revitron.DOC, element, intersected):
+				try:
+					if _(intersected).isNotOwned:
+						revitron.DB.JoinGeometryUtils.JoinGeometry(revitron.DOC, element, intersected)
+					else:
+						print('Element {} is used by another user!'.format(output.linkify(_elem.Id)))
+				except:
+					pass
+
+transaction.commit()
