aff4f386ffdb2c60e023641d47db0dc7357863f7
Marc Anton Dahmen <revitron@marcdahmen.de>
2021-03-29 11:54:22 +0200

    Update About_script.py

--- Revitron.tab/Revitron.panel/About.pushbutton/About_script.py
+++ Revitron.tab/Revitron.panel/About.pushbutton/About_script.py
@@ -1,9 +1,17 @@
 import revitron
+import os
+import datetime
 from pyrevit import script
 from pyrevit import output
-import os
 
+now = datetime.datetime.now()
 parent = os.path.dirname 
+html = """<div style="padding: 1rem 1rem"><h2>Revitron</h2>A Revit API wrapper written in Python.
+Check out the <a href="https://revitron-ui.readthedocs.io/">documentation</a>, the <a href="https://revitron.readthedocs.io/">API reference</a>
+and the repositories on <a href="https://github.com/revitron">GitHub</a> for more infos.
+
+&copy; Copyright 2020-{}, Marc Anton Dahmen &mdash; MIT license
+All icons except for the Revitron logo by <a href="https://icons8.com">Icons8</a></div>"""
 
 svg = parent(parent(parent(parent(__file__)))) + '/svg/revitron-about.svg'
 
@@ -13,10 +21,4 @@ output.get_output().set_width(500)
 output.get_output().set_height(500)
 out = script.get_output()
 out.print_image(svg)
-out.print_html('<div style="padding: 1rem 1rem"><h2>Revitron</h2>A Revit API wrapper written in Python.<br>' +\
-	'Check out the <a href="https://revitron-ui.readthedocs.io/">documentation</a>, ' +\
-    'the <a href="https://revitron.readthedocs.io/">API reference</a><br>' +\
-    'and the repositories on <a href="https://github.com/revitron">GitHub</a> for more infos.<br><br>' +\
-    '&copy; Copyright 2020, Marc Anton Dahmen &mdash; MIT license<br>' +\
-    'All icons except for the Revitron logo by <a href="https://icons8.com">Icons8</a></div>')
-
+out.print_html(html.format(now.year))
